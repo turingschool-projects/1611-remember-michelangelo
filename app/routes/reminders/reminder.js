3250be77095b11b40b95232c195dfcf2dee4681c
Kenia Farias <11431kenia@gmail.com>
2017-04-04 19:44:35 -0600

    adds reminder.js file into reminder folder

--- app/routes/reminders/reminder.js
+++ app/routes/reminders/reminder.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.get('store').find('reminder', params.reminder_id)
+  model(params) {
+    return this.get('store').find('reminder', params.reminder_id);
   }
 });
